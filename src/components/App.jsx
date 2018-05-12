bf9ce2995a600e2c2ed9696dbd55fd571ffc8fc6
Ivaylo Zhekov <Ivaylo_Zhekov@epam.com>
2018-05-12 13:12:35 +0300

    adding aframe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
+import 'aframe';
+// import 'aframe-particle-system-component';
+import 'aframe-orbit-controls-component-2';
+import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import 'assets/scss/App.scss';
-import reactLogo from 'assets/img/react_logo.svg';
 
 class App extends React.PureComponent {
   render() {
     return (
-        <div className="app">
-          <h1>Hello World!</h1>
-          <p>Foo to the bar</p>
-          <img src={reactLogo} height="480"/>
-        </div>
+        <Scene background="color: grey">
+            <Entity
+              id="camera"
+              camera
+              position="0 0 5"
+              orbit-controls="autoRotate: false; target: #target; enableDamping: true; dampingFactor: 0.25; rotateSpeed:0.14; minDistance:3; maxDistance:15;"
+              mouse-cursor=""
+          />
+          <Entity id="target" />
+          <Entity primitive="a-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2000px-Adidas_Logo.svg.png" />
+          <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+          <Entity particle-system={{preset: 'snow'}}/>
+          <Entity light={{type: 'point'}}/>
+          <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
+        </Scene>
     );
   }
 }
