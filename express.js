70183e2a82448af2c0deeadc5ac18abc98f15331
noname <no_name@noname.com>
2018-05-12 18:34:06 +0300

    shoe

--- express.js
+++ express.js
@@ -3,9 +3,24 @@ const app        = express();
 const portNumber = 3000;
 const sourceDir  = 'dist';
 
-app.use(express.static(sourceDir));
+if (process.env.PONY == 1) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-app.listen(portNumber, () => {
-  console.log(`Express web server started: http://localhost:${portNumber}`);
-  console.log(`Serving content from /${sourceDir}/`);
-});
+  app.use(express.static('src/assets'));
+
+  app.listen(8081, () => {
+    console.log(`Express web server started: http://localhost:${8081}`);
+    console.log(`Serving content from /src/assets/`);
+  });
+} else {
+  app.use(express.static(sourceDir));
+
+  app.listen(portNumber, () => {
+    console.log(`Express web server started: http://localhost:${portNumber}`);
+    console.log(`Serving content from /${sourceDir}/`);
+  });
+}
