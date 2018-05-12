8f57c50b8fbebdb6ac909cb282f62121d16b8486
noname <no_name@noname.com>
2018-05-12 15:59:50 +0300

    video component

---
+++ src/components/aframe/video-player.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent('a-video-player', {
+  schema: {},
+  init: function () {},
+  update: function () {},
+  tick: function () {
+    console.log('yolo')''
+  },
+  remove: function () {},
+  pause: function () {},
+  play: function () {}
+});
